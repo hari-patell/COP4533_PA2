4b2fff21d26101333c50def9c1eca115cc540e8b
Hunter Stewart <139908741+hstewart438@users.noreply.github.com>
2026-03-06 15:07:12 -0500

    Added in outfile creation and tests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from cache_sim import parse_input, fifo, lru, optff
 
 def main() -> None:
@@ -9,9 +10,18 @@ def main() -> None:
     except FileNotFoundError:
         print(f"File '{path}' not found.")
         sys.exit(1)
+
     print("FIFO  :", fifo(k, requests))
     print("LRU   :", lru(k, requests))
     print("OPTFF :", optff(k, requests))
+
+    # Write results to out file
+    file_name = os.path.splitext(os.path.basename(path))[0]
+    outfile = os.path.join("data", f"{file_name}.out")
+    with open(outfile, "w") as f:
+        f.write(f"FIFO  : {fifo(k, requests)}\n")
+        f.write(f"LRU   : {lru(k, requests)}\n")
+        f.write(f"OPTFF : {optff(k, requests)}")
 
 
 if __name__ == "__main__":
