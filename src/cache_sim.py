cd4a4ad8beddd70837613e4c0f200a1de7ec5264
Hunter Stewart <139908741+hstewart438@users.noreply.github.com>
2026-03-06 12:42:40 -0500

    fixed syntax error and added optff to main

--- src/cache_sim.py
+++ src/cache_sim.py
@@ -38,6 +38,44 @@ def lru(k: int, requests: list[int]) -> int:
         cache[x] = None
     return misses
 
+def optff(k, requests):
+    capacity = k
+    cache = []
+    misses = 0
+    m = len(requests)
+
+    # use enumerate to reduce loops, like dict in PA1
+    for idx, req in enumerate(requests):
+        # check cache hit
+        if req in cache:
+            continue
+        else:
+            misses += 1
+
+            # cache has space
+            if len(cache) < capacity:
+                cache.append(req)
+                continue
+
+            # cache full
+            farthest = -1
+            evicted = None
+
+            # loop and keep track of the cache item that is current farthest
+            # index gives next occurence of cache item in requests
+            for item in cache:
+                used = requests.index(item, idx+1)
+                
+                if used > farthest:
+                    farthest = used
+                    evicted = item
+
+            # update
+            cache.remove(evicted)
+            cache.append(req)
+
+    return misses
+
 
 if __name__ == "__main__":
     k, requests = parse_input("data/example.in")
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import sys
-from cache_sim import parse_input, fifo, lru
+from cache_sim import parse_input, fifo, lru, optff
 
 def main() -> None:
     path = sys.argv[1] if len(sys.argv) > 1 else "data/example.in"
     k, requests = parse_input(path)
     print("FIFO :", fifo(k, requests))
     print("LRU  :", lru(k, requests))
+    print("LRU  :", optff(k, requests))
 
 
 if __name__ == "__main__":
